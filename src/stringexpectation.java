d6e0bef7fcbdbc8639ea5d85c0fd6561019c60a5
21sneha <snehabarbie1998@gmail.com>
2021-10-19 20:25:53 +0530

    first commit

---
+++ src/stringexpectation.java
@@ -0,0 +1,15 @@
+public class stringexpectation {
+    public static void main(String args[])
+    {
+        try{
+            String str="beginnersbook";
+            System.out.println(str.length());;
+            char c = str.charAt(0);
+            c = str.charAt(40);
+            System.out.println(c);
+        }catch(StringIndexOutOfBoundsException e){
+            System.out.println("StringIndexOutOfBoundsException!!");
+        }
+    }
+}
+
